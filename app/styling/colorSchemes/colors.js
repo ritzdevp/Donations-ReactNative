4dc1e69274145abdef45d8b67534f6c56bdb4c95
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-09-08 15:27:43 +0530

    added colors.js

---
+++ app/styling/colorSchemes/colors.js
@@ -0,0 +1,10 @@
+const {exp} = require('react-native-reanimated');
+
+export default {
+  primary: '#3954FD', // blue
+  secondary: '#396DFE', // lightblue
+  grey: '#9C9A7E', // grey
+  offwhite: '#EBEFF2', // offwhite
+  white: '#FFFFFF',
+  black: '#000000',
+};
