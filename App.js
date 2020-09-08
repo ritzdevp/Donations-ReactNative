ae96090cf2b07c7d588eff52691c7aea6e685eb7
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-09-08 18:48:59 +0530

    todo-navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Navigator from './app/routes/SchoolRequirementsStack';
+//todo-navigation
 
 export default function App() {
   return <Navigator />;
