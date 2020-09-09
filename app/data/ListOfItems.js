5c58355f8e74b379a319973d2310c3f3934e0c5b
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-09-09 17:50:42 +0530

    create items flatlist

---
+++ app/data/ListOfItems.js
@@ -0,0 +1,26 @@
+
+export default {
+
+  DATA = [
+  {
+    id: '1',
+    title: 'Accessories',
+    image: '../styling/images/accessories.png',
+  },
+  {
+    id: '2',
+    title: 'Bags',
+    image: '../styling/images/bag 1.png',
+  },
+  {
+    id: '3',
+    title: 'Transportations',
+    image: '../styling/images/bus 2.png',
+  },
+  {
+    id: '4',
+    title: 'Copies',
+    image: '../styling/images/copies 1png',
+  },
+]
+};
