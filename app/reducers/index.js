d6a3a8397bea17e4728cac186297fffbd9cdd690
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-09-23 03:21:48 +0530

    checking branch

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import selectedItemsList from './selectedItemsList';
 
 export default combineReducers({
   selectedItemsList,
 });
+
+//
