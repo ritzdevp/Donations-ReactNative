8fe0b2dc32f9745e37f88e3c10ddadddfa6f0854
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-10-28 17:06:45 +0530

    ignored virtualizedlist nested error

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import 'react-native-gesture-handler';
 //import Navigator from './app/routes/drawer';
 import Navigator from './app/routes/drawer';
 import {NavigationContainer} from '@react-navigation/native';
-import SchoolRequirements from './app/screens/SchoolRequirements';
-// import SchoolDetailsForm from './app/screens/SchoolDetailsForm';
-// import AppBanner from './app/components/AppBanner';
-// import WelcomeScreen from './app/screens/WelcomeScreen';
-// import DonorSchoolList from './app/screens/DonorSchoolList';
-// import SchoolRequirements from './app/screens/SchoolRequirements';
+import { LogBox } from 'react-native';
 
 export default function App() {
   return <Navigator />;
 }
+
+//Ignored Errors
+LogBox.ignoreLogs(['VirtualizedLists should never be nested inside']); //This is caused due to FlatList present inside ScrollView
+//Solution (TODO): Remove ScrollView and make use of ListHeaderComponent and ListFooterComponent parameters of FlatList.
+
