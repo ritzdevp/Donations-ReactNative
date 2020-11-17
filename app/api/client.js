0f136b552ff6e3a4216fc586fbb48637255cbd4a
Rituraj Singh <rituraj.singh@accoliteindia.com>
2020-11-17 12:41:29 +0530

    changed backend link

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,8 @@
 import {create} from 'apisauce';
 
 const apiClient = create({
-  baseURL: 'https://backendherokudonorapp.herokuapp.com/api',
+  baseURL: 'https://donorappbackend.herokuapp.com/api', 
+  //CHANGE THIS URL TO THE ONE (deployed on 10 Nov 2020) USED IN THE ADMIN TOOL
   //IP1: 192.168.137.1
   //IP2: 192.168.0.108
   //http://192.168.43.145:8000/api
